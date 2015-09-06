0b317f57ecc6507443700524259f46cd3efa63c4
Daniel Nephin <dnephin@gmail.com>
2015-09-06 12:51:04 -0400

    Move slave discovery configuration to environment variables so it can be changed without rebuilding the image.

--- swarm-slave/start.sh
+++ swarm-slave/start.sh
@@ -7,14 +7,16 @@ port=2375
 
 # Get the ip address of the discovery service, so it can be passed to the
 # swarm slave container
-zk_host=$(getent hosts zookeeper | awk '{ print $1 }' | head -1)
-discovery="zk://$zk_host:2181/"
+discovery_ip=$(getent hosts $discovery_host | awk '{ print $1 }' | head -1)
 
 # Start docker daemon
 /dind docker daemon -H 0.0.0.0:$port &
 sleep 2
 
 # Start swarm slave
-DOCKER_HOST=tcp://localhost:$port docker run swarm join --addr=$host:$port $discovery
+DOCKER_HOST=tcp://localhost:$port docker run \
+    --add-host $discovery_host:$discovery_ip \
+    swarm join \
+    --addr=$host:$port $discovery
 
 # TODO: doesn't exit properly on SIGTERM
